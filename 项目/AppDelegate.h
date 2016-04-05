4a56636a31437d2c4e0d5cc7a6a2eb63de272f14
ma c <mac@zhaoyunhong.local>
2016-04-05 15:31:34 +0800

    Initial Commit

---
+++ 项目/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  项目
+//
+//  Created by ma c on 16/4/5.
+//  Copyright © 2016年 swl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 项目/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  项目
+//
+//  Created by ma c on 16/4/5.
+//  Copyright © 2016年 swl. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
